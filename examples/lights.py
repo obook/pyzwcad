d59b7534a05805d5e8f2b8c98fa103377f6323b3
øbook <obooklage@free.fr>
2025-03-15 04:46:29 +0100

    Work about lights example

--- examples/lights.py
+++ examples/lights.py
@@ -1,35 +1,49 @@
 #/usr/bin/env python
 #-*- coding: utf-8 -*-
+"""
+    lights.py
+    ~~~~~~~~~~~~~~~
+
+    example : scan MText and MLeader on draw
+
+    :copyright: (c) 2012 by Roman Haritonov.
+    :copyright: (c) 2025 by obook.    
+    :license: BSD, see LICENSE.txt for more details.
+"""
 import re
 import sys
 from collections import namedtuple, defaultdict
 
-from pyzwcad.pyautocad import Autocad
-from pyzwcad.pyautocad import utils
+from pyzwcad import ZwCAD, utils
 
 LampEntry = namedtuple('LampEntry', 'number, mark, numxpower')
 
 # \A1;2ARCTIC SMC/SAN 254 \S2х54/2,5;\P300 лк
-def iter_lamps(acad, objects):
-    for obj in acad.iter_objects(('MText', 'MLeader'), block=objects):
+# '1/MARK1/23/LED;'
+def iter_lamps(zwcad, objects):
+    for obj in zwcad.iter_objects(('MText', 'MLeader'), block=objects):
         try:
             text = obj.TextString
         except Exception:
             continue
         text = utils.unformat_mtext(text)
-        m = re.search('(?P<num>\d+)(?P<mark>.*?)\\S(?P<num_power>.*?)/.*?;', text)
+        
+        print(f"unformat_mtext = [{text}]")
+        
+        m = re.search(r'(?P<num>\d+)(?P<mark>.*?)\\S(?P<num_power>.*?)/.*?;', text)
+        # m = re.search(r'(?P<num>\d+)\/(?P<mark>.*?)\/(?P<num_power>.*?)\/.*?;', text)
         if not m:
             continue
         print(m.group('num'), m.group('mark'), m.group('num_power'))
         yield LampEntry(m.group('num'), m.group('mark'), m.group('num_power'))
 
 def main():
-    acad = Autocad()
+    zwcad = ZwCAD()
     objects = None
     if 'i' in sys.argv[1:]:
-        objects = acad.get_selection('Select objects')
+        objects = zwcad.get_selection('Select objects')
     lamps = defaultdict(int)
-    for lamp in iter_lamps(acad, objects):
+    for lamp in iter_lamps(zwcad, objects):
         lamps[lamp.mark] += int(lamp.number)
     print('-' * 79)
     for mark, number in sorted(lamps.items()):
